e6a9e52e95fc9cbafb85db6d3e23a48f2d62badc
fidel <fidelvzcs@gmail.com>
2026-04-18 12:18:26 +0200

    U4EJ1 reverseArr finished

--- U4/U4EJ1.js
+++ U4/U4EJ1.js
@@ -16,13 +16,13 @@ console.log(reverseArray(4,7,21,3)); //This will return [3,21,7,4]
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
 
-function reverseArray(...numbers){
-  let reverse = [];
-  for (let i = numbers.length -1; i >= 0; i--){
-    reverse.push(numbers[i]);
-  }
+function reverseArray(...numbers) {
+	let reverse = [];
+	for (let i = numbers.length - 1; i >= 0; i--) {
+		reverse.push(numbers[i]);
+	}
 
-  return reverse;
+	return reverse;
 }
 
 
